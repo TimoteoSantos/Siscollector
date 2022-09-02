43805fca8fe070d054e30c4501381ab75a919de6
timoteo santos <santostimoteo21@gmail.com>
2022-09-02 17:53:17 -0300

    atualizar black vendas

--- coletar_mobile/mensagens.php
+++ coletar_mobile/mensagens.php
@@ -1,31 +1,19 @@
-<?php     
-
-								
-			 			if(isset($_SESSION['msg'])){
-			              echo $_SESSION['msg'];
-			              unset($_SESSION['msg']);
-			              }
-			              if(isset($_SESSION['msg2'])){
-			              echo $_SESSION['msg2'];
-			              unset($_SESSION['msg2']);
-			              }
-
+<?php
 						  $usuario = $_SESSION['usuario'];
-						  	
 
-						    $l = mysqli_query($conexao,"SELECT id, quantidade FROM coletor_importar where usuario = '$usuario' ORDER BY id DESC LIMIT 1 ;");
+						    $l = mysqli_query($conexao,"SELECT max(id) as id, quantidade FROM coletor_importar where usuario = '$usuario' ORDER BY id DESC LIMIT 1 ;");
 
 						   while($linha2 = mysqli_fetch_array($l)) {
 
 						    $alerta = $linha2[1];
 
+						  if(isset($_SESSION['msg'])){
+			              echo $_SESSION['msg'];
+			              unset($_SESSION['msg']);
+			              }
+
 						    if ($alerta > 50){
 
 						    echo "<p class='alerta'> OBSERVE A QUANTIDADE ! = '$alerta'<audio src='erro.mp3' autoplay></audio> </p>";
-
 					  }
-
-					   }
-
-
-					?>
+				   }
